60c7e4f421ffb62cbcbc89ed7f9d57cb10006e08
loic_dev <loic.work@gmail.com>
2016-09-15 18:28:14 +0200

    add Rabbit Worker for node

---
+++ nodejs_poc/src/worker/new_task.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var q = 'nodejs_worker';
+    var msg = process.argv.slice(2).join(' ') || "Hello World!";
+
+    ch.assertQueue(q, {durable: true});
+    for(var i = 0; i < 100000;i++) {
+
+      ch.sendToQueue(q, new Buffer(msg), {persistent: true});
+      console.log(" [x] Sent '%s' no %s", msg, i);
+    }
+  });
+  setTimeout(function() { conn.close(); process.exit(0) }, 500);
+});
