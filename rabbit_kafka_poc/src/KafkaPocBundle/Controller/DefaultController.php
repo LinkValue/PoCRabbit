a450652e982df5b0dbc451874e67b6856489b19f
Julien Bouffard <bouffard.jul@gmail.com>
2016-10-21 12:08:24 +0200

    WIP pipe stream

--- rabbit_kafka_poc/src/KafkaPocBundle/Controller/DefaultController.php
+++ rabbit_kafka_poc/src/KafkaPocBundle/Controller/DefaultController.php
@@ -13,7 +13,7 @@ class DefaultController extends Controller
         $rk->setLogLevel(LOG_DEBUG);
         $rk->addBrokers("127.0.0.1");
 
-        $topic = $rk->newTopic("k");
+        $topic = $rk->newTopic("a");
         for ($i=0; $i < 200; $i++) { 
             $topic->produce(RD_KAFKA_PARTITION_UA, 0, "Message payload".$i);
         }
@@ -29,7 +29,7 @@ class DefaultController extends Controller
         $rk = new \RdKafka\Consumer();
         $rk->setLogLevel(LOG_DEBUG);
         $rk->addBrokers("127.0.0.1"); 
-        $topic = $rk->newTopic("k");
+        $topic = $rk->newTopic("a");
 
         $topic->consumeStart(0, RD_KAFKA_OFFSET_BEGINNING);
         $errors = [];
@@ -54,11 +54,14 @@ class DefaultController extends Controller
 
     public function dlAction()
     {
-        $handle = fopen('http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4', 'rb');
-        $chunkSize = 1024; // The size of each chunk to output
-        $position = 
+        $handle = fopen('https://twitrss.me/twitter_user_to_rss/?user=linkvalue', 'rb');
+        $chunkSize = 56; // The size of each chunk to output
  
-    
+        $errors = [];
+        $messages = [];
+        $i = 0;
+        $start = time();
+
         $rk = new \RdKafka\Producer();
         $rk->setLogLevel(LOG_DEBUG);
         $rk->addBrokers("127.0.0.1");
@@ -69,10 +72,16 @@ class DefaultController extends Controller
             $topic->produce(RD_KAFKA_PARTITION_UA, 0, $buffer);
             ob_flush();
             flush();
+            $messages['nb message'] = $i++;
         }
         fclose($handle);
+        $end = time();
+        $messages['time elapsed'] = $start-$end;
         
-        return new Response();
+        return $this->render('KafkaPocBundle:Default:index.html.twig', [
+                'errors' => $errors,
+                'messages' => $messages
+            ]);
     }
 
     public function fullMovieAction()
@@ -86,7 +95,7 @@ class DefaultController extends Controller
         $errors = [];
         $messages = [];
         $movie = '';
-        $handle = fopen(__DIR__.'/movie.txt', 'w+');
+        $handle = fopen(__DIR__.'/../../movie.txt', 'w+');
         
         while (true) {
             // The first argument is the partition (again).
@@ -111,5 +120,58 @@ class DefaultController extends Controller
                 'errors' => $errors,
                 'messages' => $messages
             ]);
+    }
+
+    public function streamLogAction()
+    {
+        $handle = fopen(__DIR__.'/../../../var/logs/dev.log', 'r');
+        //var_dump($handle); 
+        $rk = new \RdKafka\Producer();
+        $rk->setLogLevel(LOG_DEBUG);
+        $rk->addBrokers("127.0.0.1");
+
+        // $topic = $rk->newTopic("streams-pipe");
+        // while (($buffer = fgets($handle, 4096)) !== false) {
+        //     //$topic->produce(RD_KAFKA_PARTITION_UA, 0, $buffer);
+        // }
+        fclose($handle);
+        return $this->render('KafkaPocBundle:Default:stream_log.html.twig'); 
+    }
+
+    public function showLogAction()
+    { 
+        return $this->render('KafkaPocBundle:Default:show_log.html.twig');  
+    }
+
+    public function getStreamedLogAction()
+    {
+        $conf = new \RdKafka\Conf();
+        $conf->set('formatter', 'kafka.tools.DefaultMessageFormatter');
+        // $conf->set('print.key', true);
+        // $conf->set('print.value', true);
+        // $conf->set('key.deserializer', 'org.apache.kafka.common.serialization.StringDeserializer');
+        // $conf->set('value.deserializer', 'org.apache.kafka.common.serialization.StringDeserializer');
+
+        $rk = new \RdKafka\Consumer($conf);
+        $rk->setLogLevel(LOG_DEBUG);
+        $rk->addBrokers("127.0.0.1");
+
+        $topic = $rk->newTopic("streams-pipe-output");
+        $topic->consumeStart(0, RD_KAFKA_OFFSET_BEGINNING);
+        $errors = [];
+        $messages = [];
+        while (true) {
+            // The first argument is the partition (again).
+            // The second argument is the timeout.
+            $msg = $topic->consume(0, 1000);
+            if ($msg->err) {
+//                $errors[] = $msg->errstr(). "\n";
+                break;
+            } else {
+                $messages[] = $msg->payload;
+            }
+        }
+
+        return new JsonResponse($messages);
     }
 }
